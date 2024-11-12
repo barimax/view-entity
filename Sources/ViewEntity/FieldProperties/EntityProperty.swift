3ca184db10ca3e3cd42e14dd8006eaae07789367
Georgie Ivanov <georgie@barimax.bg>
2024-11-12 13:49:10 +0200

    Add EntitySimpleProperty

--- Sources/ViewEntity/FieldProperties/EntityProperty.swift
+++ Sources/ViewEntity/FieldProperties/EntityProperty.swift
@@ -54,3 +54,49 @@ public struct EntityProperty<T, V>: FieldProtocol where T: Model, V: Codable, V:
         self.init(keyField: "id", name: "id", fieldType: .hidden, dataType: .string, label: "id", order: 0)
     }
 }
+
+public struct EntitySimpleProperty: FieldProtocol {
+    public let keyField: String
+    public let name: String
+    public var fieldType: FieldType
+    public let dataType: DataType
+    public let label: String
+    public var required: Bool
+    public var disabled: Bool
+    public let order: Int
+    public let width: Int
+    public let ref: OptionableProtocol.Type?
+    public let nestedRef: RefViewEntityProtocol.Type?
+    public let agregate: [Agregate]?
+    
+    public init(keyField: String,
+         name n: String,
+         fieldType f: FieldType,
+         dataType t: DataType,
+         label l: String,
+         order o: Int,
+         required r: Bool = true,
+         ref rf: OptionableProtocol.Type? = nil,
+         width w: Int = 200,
+         disabled d: Bool = false,
+         nestedRef nr: RefViewEntityProtocol.Type? = nil,
+         agregate isa: [Agregate]? = nil
+    ){
+        self.keyField = keyField
+        self.name = n
+        self.fieldType = f
+        self.dataType = t
+        self.label = l
+        self.required = r
+        self.disabled = d
+        self.width = w
+        self.order = o
+        self.ref = rf
+        self.nestedRef = nr
+        self.agregate = isa
+    }
+    
+    static func id() -> EntitySimpleProperty {
+        self.init(keyField: "id", name: "id", fieldType: .hidden, dataType: .string, label: "id", order: 0)
+    }
+}
