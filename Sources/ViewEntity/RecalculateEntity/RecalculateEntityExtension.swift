7d9fd58b314469f65b48a55d696d74277305763d
Georgie Ivanov <georgie@barimax.bg>
2026-08-05 12:25:25 +0300

    Fix recalculted DTO

--- Sources/ViewEntity/RecalculateEntity/RecalculateEntityExtension.swift
+++ Sources/ViewEntity/RecalculateEntity/RecalculateEntityExtension.swift
@@ -11,6 +11,7 @@ import Fluent
 /// If entity is recalclatable and conforms to DTO protocol
 /// Calls entity recalculate static method after entity creation with transaction database object supplied
 /// Returns DTO object from entity after create and update transactions used in save static method
+
 public extension EntityModelProtocol where Self == Self.DTO.M, Self: RecalculateProtocol {
     
     static func createTransaction(createdEntity: Self, database: Database, request: Request, view: inout (any ViewEntityProtocol)?) async throws -> EntityCodable {
@@ -23,6 +24,12 @@ public extension EntityModelProtocol where Self == Self.DTO.M, Self: Recalculate
     static func updateTransaction(oldEntity: Self, newEntity: Self, database: Database, request: Request) async throws -> EntityCodable {
         try await Self.update(oldEntity: oldEntity, newEntity: newEntity, database: database, request: request)
         return try DTO.fromModel(entity: newEntity)
+    }
+    
+    static func recalculate(request: Request) async throws -> (any Encodable, (any ViewEntityProtocol)?) {
+        let dto = try request.content.decode(DTO.self)
+        let (recalculatedDTO, view) = try await Self.recalculate(request: request, dto: dto)
+        return (recalculatedDTO, view)
     }
 }
 public extension EntityModelProtocol where Self: RecalculateTriggersProtocol {
